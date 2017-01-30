2c158b391d3bf222f4fdd756a1854ec2dfb7162e
bgrogg <borisgrogg@Boriss-MacBook-Air.local>
2017-01-30 12:20:19 -0800

    preliminary go code for chat client, no front-end rendering yet

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById('input');
+  const messages = document.getElementById('messages');
+  const button = document.getElementById('chat-button');
+  button.onclick =
+
+  let ws = new WebSocket
+})
