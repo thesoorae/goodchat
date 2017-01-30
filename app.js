16adb3c8b8745d3fbb84235167e5366b139e3bfd
The Soo-Rae Hong <the.soorae@gmail.com>
2017-01-30 14:35:43 -0800

    app js preliminary code

--- app.js
+++ app.js
@@ -4,7 +4,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById('input');
   const messages = document.getElementById('messages');
   const button = document.getElementById('chat-button');
-  button.onclick =
-
-  let ws = new WebSocket
+  
 })
