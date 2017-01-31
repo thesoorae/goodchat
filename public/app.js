e7579f9c71e2e3ff18cc47023c5c130749b4a63c
bgrogg <borisgrogg@Boriss-MacBook-Air.local>
2017-01-30 18:02:11 -0800

    started to figure out websockets

--- public/app.js
+++ public/app.js
@@ -7,11 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let conn = null;
 
     function appendLog(item) {
-
-        log.appendChild(item);
-
+      log.appendChild(item);
     }
+
+
     button.onclick = function () {
+      console.log("clicked");
         if (!conn) {
             return false;
         }
@@ -23,13 +24,16 @@ document.addEventListener("DOMContentLoaded", () => {
         return false;
     };
     if (window["WebSocket"]) {
+      console.log("in websocket");
         conn = new WebSocket('ws://' + window.location.host + '/ws');
         conn.onclose = function (evt) {
+          console.log("in onclose");
             var item = document.createElement("div");
             item.innerHTML = "<b>Connection closed.</b>";
             appendLog(item);
         };
         conn.onmessage = function (evt) {
+          console.log("in onmessage");
             var messages = evt.data.split('\n');
             for (var i = 0; i < messages.length; i++) {
                 var item = document.createElement("div");
