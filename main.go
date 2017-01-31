2c17d20762e5ad5b6d1d3497d90c7f7c88b87924
The Soo-Rae Hong <the.soorae@gmail.com>
2017-01-31 15:51:43 -0800

    styling

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+
+
 import (
   "log"
   "net/http"
   "github.com/gorilla/websocket"
   "os"
 )
+
+// import (
+//   "log"
+//   "net/http"
+//   "github.com/gorilla/websocket"
+//
+// )
 
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Message)
